cd596df1c15eb52b232b8ca8d02b05e01c2b3d20
lost-fox <Oljaburak@mail.ru>
2022-06-26 17:23:51 +0300

    feat: add valid name

--- form-fe/src/form/index.tsx
+++ form-fe/src/form/index.tsx
@@ -1,20 +1,51 @@
+import React, { useState } from "react";
 import "./style.scss";
 
 export const Forms: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [isValidName, setValidName] = useState(false);
+  const [errorName, setErrorName] = useState("");
+
+  const colorErrorLabel = (errorMessage: string) => {
+    const color = !errorMessage ? "#000000" : "#db2a2a";
+    return color;
+  };
+
+  const validName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.value;
+    const reg = /^\s?[a-zA-Z]{3,30}\s([a-zA-Z]{3,30})$/i.test(name);
+    if (!reg) {
+      setErrorName(
+        "Поле должно состоять из 2-х слов латинского алфавита, длинной от 3 до 30 символов и 1 пробел между словами"
+      );
+      setValidName(false);
+      if (!name.length) setErrorName("");
+    } else {
+      setErrorName("");
+      setValidName(true);
+    }
+
+    setUsername(name.toUpperCase());
+  };
+
   return (
     <div className="container-form">
       <h1 className="form-title">Обратная связь</h1>
-      <form noValidate>
+      <form>
         <div>
-          <label htmlFor="name">Имя и Фамилия </label>
+          <label htmlFor="name" style={{ color: colorErrorLabel(errorName) }}>
+            Имя и Фамилия
+          </label>
           <br />
           <input
             type="text"
             name="name"
             id="name"
             placeholder="Введите имя и фамилию"
+            value={username}
+            onChange={validName}
           />
-          <p className="form-error">Error</p>
+          <p className="form-error">{errorName}</p>
         </div>
         <div>
           <label htmlFor="email">E-mail </label> <br />
