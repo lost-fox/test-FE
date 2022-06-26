811b60c78bf80c143849e3b1fb2acc1226674d8a
lost-fox <Oljaburak@mail.ru>
2022-06-26 15:38:57 +0300

    feat: add linter and button

--- form-fe/src/App.tsx
+++ form-fe/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container-app">
       <Forms />
-    </div>  
+    </div>
   );
 }
 
--- form-fe/src/form/index.tsx
+++ form-fe/src/form/index.tsx
@@ -1,31 +1,58 @@
 import "./style.scss";
 
-export const Forms:React.FC = () => {
-   return (
-      <div className='container-form'> 
-         <h1 className='form-title'>Обратная связь</h1>
-         <form>
-            <div>
-               <label htmlFor="name">Имя и Фамилия </label> <br/>
-               <input type="text" name="name" id="name" />
-            </div>
-            <div>
-              <label htmlFor="email">Email </label> <br/>
-              <input type="email" name="email" id="email" /> 
-            </div>
-            <div>
-               <label htmlFor="phone">Номер телефона </label> <br/>
-               <input type="tel" name="phone" id="phone" />
-            </div>
-            <div>
-               <label htmlFor="date">Дата рождения </label> <br/>
-               <input type="date" name="date" id="date" />
-            </div>
-            <div>
-               <label htmlFor="message">Оставьте свое сообщение</label> <br/>
-                <textarea name="message" id="message"></textarea>
-            </div>
-         </form>
-      </div>
-   )
-}
+export const Forms: React.FC = () => {
+  return (
+    <div className="container-form">
+      <h1 className="form-title">Обратная связь</h1>
+      <form noValidate>
+        <div>
+          <label htmlFor="name">Имя и Фамилия </label>
+          <br />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Введите имя и фамилию"
+          />
+          <p className="form-error">Error</p>
+        </div>
+        <div>
+          <label htmlFor="email">E-mail </label> <br />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Введите E-mail"
+            formNoValidate
+          />
+          <p className="form-error">Error</p>
+        </div>
+        <div>
+          <label htmlFor="phone">Номер телефона </label> <br />
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            placeholder="Введите номер телефона"
+          />
+          <p className="form-error">Error</p>
+        </div>
+        <div>
+          <label htmlFor="date">Дата рождения </label> <br />
+          <input type="date" name="date" id="date" />
+          <p className="form-error">Error</p>
+        </div>
+        <div>
+          <label htmlFor="message">Оставьте свое сообщение</label> <br />
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Введите сообщение"
+          ></textarea>
+          <p className="form-error">Error</p>
+        </div>
+        <button className="send-btn">Отправить</button>
+      </form>
+    </div>
+  );
+};
